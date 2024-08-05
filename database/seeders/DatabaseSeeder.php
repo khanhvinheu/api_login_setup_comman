5a2c28ba35bef80af07592bfb1eeddb1dce0245b
DIAMOND\vinh.du <vinh.khanhdu@gmail.com>
2024-08-05 16:02:45 +0700

    add sh file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(Module::class);
         $this->call(Role::class);
         $this->call(Users::class);
-        $this->call(Province::class);      
+        // $this->call(Province::class);      
     }
 }
